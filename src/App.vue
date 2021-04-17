201a8d74c21e139e115260ca895703b9575104cd
BSalikhov <bakhadirsaalikhov@mail.ru>
2021-04-18 02:02:58 +0500

    feat: added gpa calculator

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,126 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="overflow-x-auto bg-gray-200">
+    <div class="flex flex-col justify-center h-screen md:items-center">
+      <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div
+          class="inline-block py-2 mx-auto align-middle min-w-1/3 sm:px-6 lg:px-8"
+        >
+          <div>
+            <label
+              for="location"
+              class="block text-sm font-medium text-gray-700"
+              >Guruhingizni tanlang</label
+            >
+            <select
+              v-model="selectedGroup"
+              id="location"
+              name="location"
+              class="block w-full py-2 pl-3 pr-10 my-1 text-base border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            >
+              <option
+                v-for="(group, $groupIndex) in groupList"
+                :key="group.name"
+                :value="$groupIndex"
+              >
+                {{ group.name }}
+              </option>
+            </select>
+          </div>
+          <div v-if="selectedGroup !== null" class="flex flex-col">
+            <div class="-my-2 sm:-mx-6 lg:-mx-8">
+              <div
+                class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8"
+              >
+                <div
+                  class="overflow-hidden border-b border-gray-200 shadow sm:rounded-lg"
+                >
+                  <table class="min-w-full divide-y divide-gray-200">
+                    <thead class="bg-gray-50">
+                      <tr>
+                        <th scope="col" class="theader">#</th>
+                        <th scope="col" class="theader">Fanlar nomi</th>
+                        <th scope="col" class="theader">
+                          Fanga ajratilgan kredit miqdori
+                        </th>
+                        <th scope="col" class="theader">Fandan olingan baho</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <TableRaw
+                        v-for="(group, $subjIndex) in groupList[selectedGroup]
+                          .subjects"
+                        :key="group.name"
+                        class="bg-white"
+                        :value="group"
+                        :id="$subjIndex"
+                        :totalCredits.sync="totalCredits"
+                      />
+                    </tbody>
+                  </table>
+                  <div
+                    class="flex items-center justify-end w-full py-2 bg-indigo-200"
+                  >
+                    <div class="tdata">
+                      Sizning GPA:
+                      <span class="font-bold text-red-700">{{
+                        (gpa / 30).toFixed(2)
+                      }}</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+// import Table from "./components/Table.vue";
+import dataList from "./assets/data/groupList.json";
+import TableRaw from "./components/TableRaw.vue";
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  components: { TableRaw },
+  data() {
+    return {
+      totalCredits: {},
+      selectedGroup: 0,
+      pointTaken: 0,
+      groupList: dataList,
+    };
+  },
+
+  computed: {
+    gpa() {
+      return Object.values(this.totalCredits).reduce(
+        (prev, curr) => prev + curr,
+        0
+      );
+    },
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.theader {
+  @apply px-6;
+  @apply py-3;
+  @apply text-xs;
+  @apply font-medium;
+  @apply tracking-wider;
+  @apply text-left;
+  @apply text-gray-500;
+  @apply uppercase;
+}
+
+.tdata {
+  @apply px-6;
+  @apply py-4;
+  @apply font-medium;
+  @apply text-gray-800;
+  @apply whitespace-nowrap;
 }
 </style>
---
+++ src/components/TableRaw.vue
@@ -0,0 +1,43 @@
+<template>
+  <tr>
+    <td class="tdata">
+      {{ id + 1 }}
+    </td>
+    <td class="tdata">
+      {{ value.text }}
+    </td>
+    <td class="tdata">
+      {{ value.credit }}
+    </td>
+    <td class="tdata">
+      <input
+        type="number"
+        min="0"
+        class="p-2 border rounded"
+        placeholder="Баллни киритинг"
+        v-model="pointTaken"
+      />
+    </td>
+  </tr>
+</template>
+
+<script>
+export default {
+  props: ["totalCredits", "value", "id"],
+  data() {
+    return {
+      pointTaken: null,
+    };
+  },
+  watch: {
+    pointTaken(point) {
+      this.$emit("update:totalCredits", {
+        ...this.totalCredits,
+        [this.id]: point * this.value.credit,
+      });
+    },
+  },
+};
+</script>
+
+<style></style>
